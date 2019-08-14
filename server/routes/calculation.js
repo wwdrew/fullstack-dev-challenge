7cfe1629d9aaf25a705b4a82f668d6fe3da68a38
Drew Miller <wwdrew@users.noreply.github.com>
2019-08-14 11:03:00 +0100

    Have the endpoint send the hardcoded data from the app

--- server/routes/calculation.js
+++ server/routes/calculation.js
@@ -1,4 +1,21 @@
 
 module.exports = function (req, res) {
-  res.send('Testing endpoint');
+  res.send([
+    {
+      month: 1,
+      amount: 500
+    },
+    {
+      month: 2,
+      amount: 700
+    },
+    {
+      month: 3,
+      amount: 1000
+    },
+    {
+      month: 4,
+      amount: 1500
+    }
+  ]);
 };
