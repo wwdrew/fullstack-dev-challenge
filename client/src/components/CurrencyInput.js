932bad59dc726b217d0077d16956094c01bd033d
Drew Miller <wwdrew@users.noreply.github.com>
2019-08-14 09:48:02 +0100

    Convert to function component

--- client/src/components/CurrencyInput.js
+++ client/src/components/CurrencyInput.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./CurrencyInput.css";
 
-export default class CurrencyInput extends Component {
-  render() {
-    const { onChange, value = 0 } = this.props;
-
-    return (
-      <div className="currency-input">
-        <span>£</span>
-        <input type="text" value={value} onChange={onChange} />
-      </div>
-    );
-  }
-}
+const CurrencyInput = ({ onChange, value = 0 }) => (
+  <div className="currency-input">
+    <span>£</span>
+    <input type="text" value={value} onChange={onChange} />
+  </div>
+);
 
 CurrencyInput.propTypes = {
   onChange: PropTypes.func.isRequired,
   value: PropTypes.number
 };
+
+export default CurrencyInput;
