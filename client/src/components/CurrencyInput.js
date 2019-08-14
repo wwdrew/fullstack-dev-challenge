d97d52c83adaba43fdcfeb68d00d1517f5f7adf3
Drew Miller <wwdrew@users.noreply.github.com>
2019-08-14 09:45:14 +0100

    Use value prop directly and set its default value when destructuring

--- client/src/components/CurrencyInput.js
+++ client/src/components/CurrencyInput.js
@@ -3,36 +3,19 @@ import PropTypes from "prop-types";
 import "./CurrencyInput.css";
 
 export default class CurrencyInput extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: props.defaultValue
-    };
-  }
-
   render() {
-    const { defaultValue, onChange } = this.props;
-    const { value } = this.state;
+    const { onChange, value = 0 } = this.props;
 
     return (
-      <div
-        className={`currency-input ${
-          defaultValue !== undefined ? "default-value" : ""
-        }`}
-      >
+      <div className="currency-input">
         <span>£</span>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
+        <input type="text" value={value} onChange={onChange} />
       </div>
     );
   }
 }
 
 CurrencyInput.propTypes = {
-  defaultValue: PropTypes.number,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number
 };
